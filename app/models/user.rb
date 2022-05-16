df545db29de784c31b2e15840ee0490d109bdf93
sCar3Cr0w25 <wretchedegg8@gmail.com>
2022-05-16 21:38:07 +0600

    fixing some redundent code

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,18 +11,9 @@ class User < ApplicationRecord
     scope :search_user_name, -> (user_name) { where(name: user_name) }
     scope :search_post_title, -> (post_title)  { joins(:posts).where("posts.title =? ", post_title) }
     scope :good_posts, -> {joins(:posts).where("posts.like_count >= ?", 10)}
-    # scope :good_posts, -> do
-    #     u = user.posts.ids
-    #     if u.length !=0
-    #         liked_posts = Post.where(id: u).where('like_count >= ?', 10).pluck(:user_id)
-    #         return good_user = User.where(id: liked_posts)
-    #     end
-    # end 
+
 
     def total_like_count
-        # c = 0
-        # self.posts.each { |s|c += s.like_count }
-        # return c
         self.posts.sum(:like_count)
     end
 end
